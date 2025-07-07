b5146a53d8538ca12af816aa26bd355e9ee29959
Şüheda KARAOSMAN <karaosmansuheda@gmail.com>
2025-07-07 13:14:19 +0300

    Update ft_print_ptr.c

--- ft_print_ptr.c
+++ ft_print_ptr.c
@@ -22,13 +22,13 @@ int	ft_print_ptr(void *ptr)
 
 	if (ptr == NULL) 
 	{
-		if (write(1, "(nil)", 5) < 0)
+		if (write(1, "(nil)", 5) == -1)
 			return (-1);
 		return (5);
 	}
 	ret = (unsigned long)ptr;
 	hex = "0123456789abcdef";
-	if (write(1, "0x", 2) < 0)
+	if (write(1, "0x", 2) == -1)
 		return (-1);
 	res = 2;
 	i = 0;
@@ -39,7 +39,7 @@ int	ft_print_ptr(void *ptr)
 	}
 	while (i-- > 0)
 	{
-		if (write(1, &buf[i], 1) < 0)
+		if (write(1, &buf[i], 1) == -1)
 			return (-1);
 		res++;
 	}
