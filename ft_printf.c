bd459b9ea1d3d23b115120882ec9f77b4a12d407
Şüheda KARAOSMAN <karaosmansuheda@gmail.com>
2025-07-10 18:36:50 +0300

    Update ft_printf.c

--- ft_printf.c
+++ ft_printf.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_printf.c                                   :+:      :+:    :+:   */
+/*   ft_printf.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: skaraosm <skaraosm@student.42kocaeli.co    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/30 18:32:19 by skaraosm          #+#    #+#             */
-/*   Updated: 2025/06/30 18:32:23 by skaraosm         ###   ########.fr       */
+/*   Updated: 2025/07/10 18:36:08 by skaraosm         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,47 +22,66 @@ static int	ft_is_conversion_specifier(char c)
 
 static int	ft_handle_conversion(va_list args, char fmt)
 {
+	int	count;
+
+	count = 0;
 	if (fmt == 'd' || fmt == 'i')
-		return (ft_print_nbr(va_arg(args, int)));
+		count = ft_print_nbr(va_arg(args, int));
 	else if (fmt == 's')
-		return (ft_print_str(va_arg(args, char *)));
+		count = ft_print_str(va_arg(args, char *));
 	else if (fmt == 'c')
-		return (ft_print_chr(va_arg(args, int)));
+		count = ft_print_chr(va_arg(args, int));
 	else if (fmt == 'p')
-		return (ft_print_ptr(va_arg(args, void *)));
+		count = ft_print_ptr(va_arg(args, void *));
 	else if (fmt == 'x')
-		return (ft_print_hex_lower(va_arg(args, unsigned int)));
+		count = ft_print_hex_lower(va_arg(args, unsigned int));
 	else if (fmt == 'X')
-		return (ft_print_hex_upper(va_arg(args, unsigned int)));
+		count = ft_print_hex_upper(va_arg(args, unsigned int));
 	else if (fmt == 'u')
-		return (ft_print_unsigned(va_arg(args, unsigned int)));
-	return (0);
+		count = ft_print_unsigned(va_arg(args, unsigned int));
+	return (count);
 }
 
 static int	ft_process_percent(va_list args, const char *format, int *i)
 {
 	int	char_count;
 
-	char_count = 0;
 	(*i)++;
 	if (!format[*i])
-	{
-		char_count += write(1, "%", 1);
-	}
+		return (write(1, "%", 1));
 	else if (format[*i] == '%')
-	{
-		char_count += ft_print_percent();
-	}
+		return (ft_print_percent());
 	else if (ft_is_conversion_specifier(format[*i]))
 	{
-		char_count += ft_handle_conversion(args, format[*i]);
+		char_count = ft_handle_conversion(args, format[*i]);
+		if (char_count == -1)
+			return (-1);
 	}
 	else
 	{
-		char_count += write(1, "%", 1);
+		char_count = write(1, "%", 1);
+		if (char_count == -1)
+			return (-1);
 		char_count += write(1, &format[*i], 1);
 	}
 	return (char_count);
+}
+
+static int	ft_process_char(va_list args, const char *format, int *i_ptr)
+{
+	int	res;
+
+	if (format[*i_ptr] == '%')
+	{
+		res = ft_process_percent(args, format, i_ptr);
+		(*i_ptr)++;
+	}
+	else
+	{
+		res = write(1, &format[*i_ptr], 1);
+		(*i_ptr)++;
+	}
+	return (res);
 }
 
 int	ft_printf(const char *format, ...)
@@ -70,30 +89,23 @@ int	ft_printf(const char *format, ...)
 	va_list	args;
 	int		i;
 	int		len;
+	int		res;
 
 	i = 0;
 	len = 0;
-    if (!(format))
-    {
-        return (-1);
-    }
+	if (!format)
+		return (-1);
 	va_start(args, format);
-    while (format[i])
+	while (format[i])
 	{
-		if (format[i] == '%')
+		res = ft_process_char(args, format, &i);
+		if (res == -1)
 		{
-			len += ft_process_percent(args, format, &i);
-			if (!format[i])
-				break ;
+			va_end(args);
+			return (-1);
 		}
-		else
-		{
-			len += write(1, &format[i], 1);
-		}
-		i++;
+		len += res;
 	}
 	va_end(args);
 	return (len);
 }
-	
-// va_start(args, format); Neden Böyle bir şey var
